15348862a8aea98c11a866edb6f179ab13940497
gover <gover@local>
2026-08-17 19:47:13 -0400

    Remover scripts locais, bats e deixar apenas arquivos publicos necessarios no GitHub

--- 1_CLIQUE_PUBLICAR_TUDO.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title Publicador Automatico do Repositorio Kodi
-cd /d "%~dp0"
-python generate_repo.py
-echo.
-pause
--- 2_CLIQUE_ATUALIZAR_DOS_AUTORES.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title Atualizador Automatico dos Autores Oficiais
-cd /d "%~dp0"
-python update_from_upstream.py
-echo.
-pause
